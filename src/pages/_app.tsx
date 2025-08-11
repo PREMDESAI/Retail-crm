194c2c5dbe2c0433a71c1172d0c080e4cd0e9806
Prem Desai <desaiprem91199@gmail.com>
2025-08-11 10:52:42 +0530

    Add demo tailwind navbar

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { Toaster } from "react-hot-toast";
+import type { AppProps } from "next/app";
+import { Provider } from "urql";
+import { client } from "../client/graphql/client";
+import Layout from "../client/components/Layout";
+import "tailwindcss/tailwind.css";
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider value={client}>
+      <Layout>
+        <Component {...pageProps} />
+        <Toaster />
+      </Layout>
+    </Provider>
+  );
+}
+
+export default CustomApp;
